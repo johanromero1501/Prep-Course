445694951914c364911f94c289c41a6bc8521558
johanromero <jhoanlokura2015@gmail.com>
2022-08-23 23:57:19 -0500

    tarea03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,4 +1,4 @@
-// No cambies los nombres de las funciones.
+ // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
@@ -69,19 +69,20 @@ function colors(color) {
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
    switch (color) {
-     case "blue":
-       return "This is blue" ;
-       
-     case "red":
+
+     case "blue":{
+       return "This is blue";
+     }
+     case "red": {
        return "This is red";
-      
-     case green:
+     }
+     case "green":{
        return "This is green";
-       
-     case "orange": 
+     }      
+     case "orange":{
        return "This is orange";
-     default:
-       return "color not found";
+     }
+     default: return "Color not found";
    }
 }
 
@@ -118,8 +119,23 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-  // De lo contrario, devuelve el numero
-}
+  // De lo contrario, devuelve el numero 
+  if (numero %3 === 0&& numero %5 === 0) {
+    return "fizzbuzz";
+   }else if (numero %3 === 0) {
+    return "fizz";
+   } else if (numero %5 ===0) {
+    return "buzz"
+   } else {
+    return numero;
+   }
+    
+   }
+  
+  
+
+  
+
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -127,8 +143,24 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-}
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  
+  if (num1 ===0 || num2 ===0 ||num3 ===0) {
+    return "Error";
+  }
+   if (num1 <0 || num2 <0 ||num3 <0) {
+    return 'Hay negativos'
+  }
+   if (num1 > num2 && num1 > num3 && num1  >0 ){ 
+    return 'Número 1 es mayor y positivo'
+  }
+   if (num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }
+   return false;
+  }
+    
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -137,7 +169,19 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
+ if (numero === 0 || numero ===1) {
+  return false;
+ }
+
+for (let i = 2; i < numero; i++) {
+  if (numero %i === 0) {
+    return false;
+  }
+ }
+
+  return true;
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -154,9 +198,12 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let array = [];
+  for (let i = 0; i < 11; i++) {
+     array.push(6*i)
+   }
+   return array;
 }
-
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
@@ -172,6 +219,15 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+var valor1 = 1;
+var resulado = numero;
+
+
+  do {
+    resulado = resulado +5;
+    valor1 = valor1 +1 ;
+  } while (valor1 <9);
+  return resulado;
 }
 
 
